900a7b60545381c428747687f910acb782fb8d81
Isaia Tonini <isaia.tonini@gmail.com>
2024-04-18 15:38:54 +0200

    step 4: added delimiter parsing and condition to regex split

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,7 +2,16 @@ function add(numbers: string): number {
 	if (numbers == "")
 		return 0;
 
-	let numList = numbers.split(/,|\n/);
+	let delim: string = '';
+	if(numbers.startsWith("//")){
+		delim = numbers.charAt(2);
+		numbers = numbers.substring(4);
+	}
+	
+	// added '\\' to RegExp to escape the delimiter and make it work for most regex reserved characters
+	let re: RegExp = new RegExp(`,|\n${delim !== '' ? '|\\' + delim : ''}`,'g');
+	let numList = numbers.split(re)
+
 	let sum = 0;
 
 	for(let n of numList){
